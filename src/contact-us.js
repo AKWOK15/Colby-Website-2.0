4ec6efe1277d9367cfdff00c1f976845ce5c00c8
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-02-23 18:50:38 -0500

    Jeez this doesn't work

--- src/contact-us.js
+++ src/contact-us.js
@@ -33,40 +33,40 @@ function ContactUs() {
 
     };
 
-    return (
-        <div className='contact-us' id='Contact Us'>
-            <header className="section-header">Contact Us</header>
-            <p className="contact-us-description">Are you a prospective student? Have a question? Want to send feedback about the website? Let us know.</p>
-            <form method="POST" name='contact' data-netlify="true" onSubmit={handleSubmit}>
-                {/* hidden input for Netlify to process submissions */}
-                <input type="hidden" name="form-name" value="contact" />
-                <label htmlFor='name'>Name</label>
-                <input type="text" id='name'  name="name" placeholder="Brian" value={name} onChange={handleName} required />
-                <label htmlFor='email'>Your email</label>
-                <input type='email' id='email' name="email" placeholder="gomules@gmail.com" value={email} onChange={handleEmail} required />
-                <label htmlFor='message'>Your message</label>
-                <textarea id='message' rows="10" columns="30" name="message" placeholder="Leave a comment" value={message} onChange={handleMessage} required></textarea>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
     // return (
     //     <div className='contact-us' id='Contact Us'>
     //         <header className="section-header">Contact Us</header>
     //         <p className="contact-us-description">Are you a prospective student? Have a question? Want to send feedback about the website? Let us know.</p>
-    //         <form method="POST" name='contact' data-netlify="true" onSubmit="submit">
+    //         <form method="POST" name='contact' data-netlify="true" onSubmit={handleSubmit}>
     //             {/* hidden input for Netlify to process submissions */}
     //             <input type="hidden" name="form-name" value="contact" />
     //             <label htmlFor='name'>Name</label>
-    //             <input type="text" id='name' placeholder="Brian" required />
+    //             <input type="text" id='name'  name="name" placeholder="Brian" value={name} onChange={handleName} required />
     //             <label htmlFor='email'>Your email</label>
-    //             <input type='email' id='email' placeholder="gomules@gmail.com" required />
+    //             <input type='email' id='email' name="email" placeholder="gomules@gmail.com" value={email} onChange={handleEmail} required />
     //             <label htmlFor='message'>Your message</label>
-    //             <textarea id='message' rows="10" columns="30" placeholder="Leave a comment" required></textarea>
+    //             <textarea id='message' rows="10" columns="30" name="message" placeholder="Leave a comment" value={message} onChange={handleMessage} required></textarea>
     //             <input type="submit" value="Submit" />
     //         </form>
     //     </div>
     // )
+    return (
+        <div className='contact-us' id='Contact Us'>
+            <header className="section-header">Contact Us</header>
+            <p className="contact-us-description">Are you a prospective student? Have a question? Want to send feedback about the website? Let us know.</p>
+            <form method="POST" name='contact' data-netlify="true" onSubmit="submit">
+                {/* hidden input for Netlify to process submissions */}
+                <input type="hidden" name="form-name" value="contact" />
+                <label htmlFor='name'>Name</label>
+                <input type="text" id='name' name="name" placeholder="Brian" required />
+                <label htmlFor='email'>Your email</label>
+                <input type='email' id='email' name="email"placeholder="gomules@gmail.com" required />
+                <label htmlFor='message'>Your message</label>
+                <textarea id='message' rows="10" columns="30" name="message" placeholder="Leave a comment" required></textarea>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
 }
 
 export default ContactUs;
