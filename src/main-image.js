c53d7e3e69525e3558d99e35143f31a0a6a9bc50
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-02-23 15:02:28 -0500

    Fixed formatting for mobile view

--- src/main-image.js
+++ src/main-image.js
@@ -1,7 +1,9 @@
 import main from './images/main.png';
+import carousel0 from './images/carousel0.jpg';
+import './main-image.css';
 function MainImage(){
  return(
-    <img src={main} className='image'></img>
+    <img src={carousel0} className='main image'></img>
  )
 }
 export default MainImage;
