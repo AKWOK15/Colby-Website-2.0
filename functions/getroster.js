16f31ece1f02cb4062f61366ebb3afbdcaa2a4b2
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-03-25 18:18:05 -0700

    Added multiple json files for multiple rosters

--- functions/getroster.js
+++ functions/getroster.js
@@ -1,12 +1,16 @@
-const r2023 = require('./roster.json')
 const headers = {
     'content-type' : 'application/json',
     'Access-Control-Allow-Origin': '*'
 };
 exports.handler = async (event) => {
-    const year = 'r' + event.queryStringParameters.year;
+    if (event.queryStringParameters.year==2023){
+           data = require('./roster2023.json');
+    }
+    else if(event.queryStringParameters.year==2022){
+        data = require('./roster2022.json');
+    }
     return {
-        body: JSON.stringify(year),
+        body: JSON.stringify(data),
         statusCode: 200,
         headers: headers
     }
