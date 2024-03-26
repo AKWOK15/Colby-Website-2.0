6e6819667c0f92faee043a4d1deae1964c1b48b2
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-03-25 21:01:17 -0700

    Fixed overflow

--- src/schedule.js
+++ src/schedule.js
@@ -33,32 +33,32 @@ function Schedule() {
     return (
         <>
             <header id='Schedule' className='section-header'>Schedule</header>
-        <div className='schedule'>
-            {/* {3000 > scroll ? (scroll > 1600 ? <img src='mitch.png' className='image active'/> : <></>) : <></>} */}
             <div className='background-container'>
-                <img src={frontRink} className={scroll < 3500 ? (scroll > 1600 ? 'image background active' : 'image background') : 'image background'} />
+                    <img src={frontRink} className={scroll < 3500 ? (scroll > 1600 ? 'image background active' : 'image background') : 'image background'} />
+                </div>
+            <div className='schedule'>
+                {/* {3000 > scroll ? (scroll > 1600 ? <img src='mitch.png' className='image active'/> : <></>) : <></>} */}
+                <table>
+                    <tbody>
+                        {game(dartmouth, 'vs', 'Dartmouth College', 'Oct 15/ 12 pm', 'T 3-3')}
+                        {game(une, 'vs', 'University of New England', 'Oct 22/ 2 pm', 'W 12-2')}
+                        {game(wpi, 'vs', 'Worcester Polytechnic Institute', 'Oct 29/ 2 pm', 'W 3-2')}
+                        {game(thomas, 'vs', 'Thomas College', 'Nov 2/ 8:30 pm', 'W 16-2')}
+                        {game(bates, 'vs', 'Bates College', 'Nov 5/ 2 pm', 'W 12-4')}
+                        {game(dartmouth, 'at', 'Dartmouth College', 'Nov 11/ 1:30 pm', 'L 3-6')}
+                        {game(bates, 'vs', 'Bates College', 'Nov 12/ 2 pm', 'W 6-2')}
+                        {game(sjc, 'at', "St. Joe's College", 'Nov 18/ 7:30 pm', 'W 12-3')}
+                        {game(cmcc, 'vs', 'Central Maine Community College', 'Nov 19/ 2 pm', 'W 2-1')}
+                        {game(sjc, 'vs', "St. Joe's College", 'Dec 3/ 2 pm', 'W 10-0')}
+                        {game(cmcc, 'vs', 'Central Maine Community College', 'Dec 6/ 8:20 pm', 'W 7-3')}
+                        {game(thomas, 'vs', 'Thomas College', 'Dec 7/ 8:30 pm', 'W 9-3')}
+                        {game(une, 'at', 'University of New England', 'Jan 27/ 3 pm', 'W 9-1')}
+                        {game(wit, 'vs', 'Wentworth Institute of Technology', 'Jan 28/ 2 pm', 'W 6-1')}
+                        {game(thomas, 'vs', 'Thomas College', 'Nov 2/ 8:30 pm', 'W 4-1')}
+                        {game(bates, 'vs', 'Bates College', 'Feb 9/ 7 pm', 'W 5-0')}
+                    </tbody>
+                </table>
             </div>
-            <table>
-                <tbody>
-                    {game(dartmouth, 'vs', 'Dartmouth College', 'Oct 15/ 12 pm', 'T 3-3')}
-                    {game(une, 'vs', 'University of New England', 'Oct 22/ 2 pm', 'W 12-2')}
-                    {game(wpi, 'vs', 'Worcester Polytechnic Institute', 'Oct 29/ 2 pm', 'W 3-2')}
-                    {game(thomas, 'vs', 'Thomas College', 'Nov 2/ 8:30 pm', 'W 16-2')}
-                    {game(bates, 'vs', 'Bates College', 'Nov 5/ 2 pm', 'W 12-4')}
-                    {game(dartmouth, 'at', 'Dartmouth College', 'Nov 11/ 1:30 pm', 'L 3-6')}
-                    {game(bates, 'vs', 'Bates College', 'Nov 12/ 2 pm', 'W 6-2')}
-                    {game(sjc, 'at', "St. Joe's College", 'Nov 18/ 7:30 pm', 'W 12-3')}
-                    {game(cmcc, 'vs', 'Central Maine Community College', 'Nov 19/ 2 pm', 'W 2-1')}
-                    {game(sjc, 'vs', "St. Joe's College", 'Dec 3/ 2 pm', 'W 10-0')}
-                    {game(cmcc, 'vs', 'Central Maine Community College', 'Dec 6/ 8:20 pm', 'W 7-3')}
-                    {game(thomas, 'vs', 'Thomas College', 'Dec 7/ 8:30 pm', 'W 9-3')}
-                    {game(une, 'at', 'University of New England', 'Jan 27/ 3 pm', 'W 9-1')}
-                    {game(wit, 'vs', 'Wentworth Institute of Technology', 'Jan 28/ 2 pm', 'W 6-1')}
-                    {game(thomas, 'vs', 'Thomas College', 'Nov 2/ 8:30 pm', 'W 4-1')}
-                    {game(bates, 'vs', 'Bates College', 'Feb 9/ 7 pm', 'W 5-0')}
-                </tbody>
-            </table>
-        </div>
         </>
     );
 }
