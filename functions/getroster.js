d50f6f70ebdcb9240301e3565e3b3f09fe406d02
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-03-25 17:22:56 -0700

    Netlify function, using json

--- functions/getroster.js
+++ functions/getroster.js
@@ -1,6 +1,12 @@
+const data = require('./roster.json')
+const headers = {
+    'content-type' : 'application/json',
+    'Access-Control-Allow-Origin': '*'
+};
 exports.handler = async () => {
     return {
-        body: "test",
-        statusCode: 200
+        body: JSON.stringify(data),
+        statusCode: 200,
+        headers: headers
     }
 }
