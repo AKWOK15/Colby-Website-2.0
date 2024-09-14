eb01b6c21bb3af65af9fbcfc024bae92a4a299f0
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-09-13 21:05:47 -0400

    Allow schedules to toggle

--- functions/getschedule.js
+++ functions/getschedule.js
@@ -6,9 +6,9 @@ exports.handler = async (event) => {
     if (event.queryStringParameters.year==2023){
            data = require('./schedule2023.json');
     }
-    // else if(event.queryStringParameters.year==2024){
-    //     data = require('./schedule2024.json');
-    // }
+    else if(event.queryStringParameters.year==2024){
+        data = require('./schedule2024.json');
+    }
     return {
         body: JSON.stringify(data),
         statusCode: 200,
