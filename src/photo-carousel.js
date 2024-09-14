ec3ffab4f5bc95979c3c3ee90c345c78b4616aa4
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-09-14 18:44:54 -0400

    Fix imports

--- src/photo-carousel.js
+++ src/photo-carousel.js
@@ -1,11 +1,10 @@
 import './photo-carousel.css';
 import { useState, useEffect, useRef, useReducer, useCallback } from 'react';
-import carousel0 from './images/pete.jpg';
+import carousel3 from './images/pete.jpg';
 import carousel1 from './images/chipgoal.jpg';
 import carousel2 from './images/scullygoal.jpg';
-import temp from './images/ANW_4189.jpg'
 function PhotoCarousel(){
-    const [photos, setPhotos]=useState([temp, carousel1, carousel2]);
+    const [photos, setPhotos]=useState([carousel1, carousel2, carousel3]);
     const [currentPhoto, setCurrentPhoto] = useState(photos[0]);
     const [count, setCount] = useState(0);
     setTimeout(()=>{
