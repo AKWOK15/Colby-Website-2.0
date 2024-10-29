1892a603e656374a0c2821580be46f51f558d9e7
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-10-29 15:08:53 -0400

    Consolidating code get schedule

--- functions/getschedule.js
+++ functions/getschedule.js
@@ -2,13 +2,14 @@ const headers = {
     'content-type' : 'application/json',
     'Access-Control-Allow-Origin': '*'
 };
+const years = [2023, 2024]
 exports.handler = async (event) => {
-    if (event.queryStringParameters.year==2023){
-           data = require('./schedule2023.json');
+    if (years.includes(event.queryStringParameters.year)){
+           data = require(`./schedule${event.queryStringParameters.year}.json`);
     }
-    else if(event.queryStringParameters.year==2024){
-        data = require('./schedule2024.json');
-    }
+    // else if(event.queryStringParameters.year==2024){
+    //     data = require('./schedule2024.json');
+    // }
     return {
         body: JSON.stringify(data),
         statusCode: 200,
