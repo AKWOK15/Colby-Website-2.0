3d2b12576c09b3904e460283e43a7c9c24587c9f
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2024-10-29 15:35:11 -0400

    Schedule 2024 complete

--- functions/getschedule.js
+++ functions/getschedule.js
@@ -4,13 +4,16 @@ const headers = {
 };
 const years = [2023, 2024];
 exports.handler = async (event) => {
-    if (years.includes(parseInt(event.queryStringParameters.year))){    
-        url = `./schedule${event.queryStringParameters.year}.json`;
-        data = require(url);
+    if(event.queryStringParameters.year==2024){
+        data = require('./schedule2024.json');
     }
-    // else if(event.queryStringParameters.year==2024){
-    //     data = require('./schedule2024.json');
+    // if (years.includes(parseInt(event.queryStringParameters.year))){    
+    //     url = `./schedule${event.queryStringParameters.year}.json`;
+    //     data = require(url);
     // }
+    else if(event.queryStringParameters.year==2023){
+        data = require('./schedule2023.json');
+    }
     return {
         body: JSON.stringify(data),
         statusCode: 200,
